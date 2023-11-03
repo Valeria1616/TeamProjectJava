5c8c9a0a8d35f3296739ff5eda5c1c84a0cbef1f
ArtVysh <vysh-artem@yandex.ru>
2023-11-03 22:15:37 +0400

    Added tests for saving

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -3,7 +3,7 @@ package ru.netology.javaqadiplom;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CreditAccountTest {
+public classgCreditAccountTest {
 
     @Test
     public void shouldAddToPositiveBalance() {
