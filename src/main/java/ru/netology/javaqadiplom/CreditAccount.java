d31c7c66f95c7ac085b33c9df5799f6da6555988
ArtVysh <vysh-artem@yandex.ru>
2023-11-08 23:34:37 +0400

    Fix #4

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -43,9 +43,9 @@ public class CreditAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        balance = balance - amount;
+
         if (balance > -creditLimit) {
-            balance = -amount;
+            balance = balance - amount;
             return true;
         } else {
             return false;
